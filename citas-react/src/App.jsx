8634b23bdf176590bf80038198f6609b9279f285
@jaquimbayoc7 <jaquimbayoc7@gmail.com>
2023-01-12 19:59:34 -0500

    Creacion del proyecto

---
+++ citas-react/src/App.jsx
@@ -0,0 +1,14 @@
+
+/*Funcion creadora de la aplicación */ 
+function App() {
+  
+  return (
+    /*Etiqueta padre y el body de la página*/
+    <> 
+      
+    </>
+    
+  )
+}
+/*Export para importar el módulo a otras partes de la app*/
+export default App
