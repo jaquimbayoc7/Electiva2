70a56998974e138ecd87d228751e8558d3bdc32c
@jaquimbayoc7 <jaquimbayoc7@gmail.com>
2023-01-16 19:24:26 -0500

    Formulario ingreso

--- citas-react/src/components/Formulario.jsx
+++ citas-react/src/components/Formulario.jsx
@@ -5,13 +5,17 @@ const Formulario = () => {
     <h2 className="font-black text-3xl text-center">
       Seguimiento a Pacientes
     </h2>
-    <p className="text-lg mt-5 text-center">
+    <p className="text-lg mt-5 text-center mb-10">
       Añade Pacientes{' '}
       <span className="text-indigo-600 font-bold">Administralos</span>
     </p>
 
-    <form action="">
-      
+    <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <div className="mb-5">
+        <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">Nombre Mascota:</label>
+
+        <input id="mascota" type="text" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" placeholder="Nombre de la mascota"/>
+      </div>
     </form>
 
   </div>
