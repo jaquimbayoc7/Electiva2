d2e6d9058e1f05b904395eab9b3ac498001ee983
@jaquimbayoc7 <jaquimbayoc7@gmail.com>
2023-01-17 19:36:10 -0500

    Componentes nuevos

---
+++ citas-react/src/components/Pacientes.jsx
@@ -0,0 +1,27 @@
+const Pacientes = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+          Nombre:{' '} <span className="font-normal normal-case">Trosky</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+          Propietario:{' '} <span className="font-normal normal-case">Julian Quimbayo</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+          Email:{' '} <span className="font-normal normal-case">correo@correo.com.co</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+          Fecha de Ingreso:{' '} <span className="font-normal normal-case">17 de Enero de 2023</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+          Síntomas:{' '} <span className="font-normal normal-case">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia doloribus vero libero fugit animi aliquam tempora, ducimus nulla quibusdam ut velit consectetur magni possimus voluptatem blanditiis maxime minus voluptas ipsa neque? Temporibus quaerat natus excepturi saepe vero! Cum, ut error?</span>
+        </p>
+      </div>
+  )
+}
+
+export default Pacientes
